7269d99d56b7ef883a26904e2a56fe99e1ead2ef
Natasha Torres <natashaxtorres@gmail.com>
2021-09-14 09:30:53 -0400

    fundamentalsReturn codewar

---
+++ week2/fundamentalsReturn.js
@@ -0,0 +1,25 @@
+//https://www.codewars.com/kata/55a5befdf16499bffb00007b
+
+function add(a, b){
+    return a + b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function modulus(a, b){
+    return a % b;
+}
+
+function exponential(a, b){
+    return a ** b;
+}
+
+function subt(a, b){
+    return a - b;
+}
