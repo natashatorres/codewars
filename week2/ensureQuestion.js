abd02157d8ad221c9f1913bc2e38f2e88ab3fe33
Natasha Torres <natashaxtorres@gmail.com>
2021-09-17 11:04:39 -0400

    codewars week 2

--- week2/ensureQuestion.js
+++ week2/ensureQuestion.js
@@ -1,10 +1,14 @@
 //https://www.codewars.com/kata/5866fc43395d9138a7000006
-
+//Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+//For example (Input --> Output)
+//"Yes" --> "Yes?" 
+//"No?" --> "No?"
 function ensureQuestion(s) {
     if(s.endsWith('?')){
       return s
     }else{
       return s.concat('?')
     }
- }
+}
+
 
