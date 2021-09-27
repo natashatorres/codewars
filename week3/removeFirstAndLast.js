4b87c172232e1692486b19756fa056f414396eb4
Natasha Torres <natashaxtorres@gmail.com>
2021-09-26 20:20:19 -0400

    week 3 codewars

---
+++ week3/removeFirstAndLast.js
@@ -0,0 +1,16 @@
+//https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/
+
+function removeChar(str){
+    return str.substring(1, str.length -1)
+  
+  };
+  
+  //paremeter string
+  //return return the the string without the first and last character
+  //example
+  //psuedo code
+  //seperate the string
+  //remove the first
+  //remove the last
+  //return it
+  
