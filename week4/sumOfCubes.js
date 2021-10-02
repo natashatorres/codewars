1183c2df58808c54987b71993838c1437ffaf16b
Natasha Torres <natashaxtorres@gmail.com>
2021-10-02 13:45:38 -0400

    week 4 codewars

---
+++ week4/sumOfCubes.js
@@ -0,0 +1,32 @@
+//https://www.codewars.com/kata/59a8570b570190d313000037
+
+
+function sumCubes(n){
+    let sum = 0;
+    for (let i = 0; i <= n; i++){
+        sum += i ** 3
+      }
+        return sum 
+}
+
+
+
+/*
+p
+takes in a positive integer
+r:
+sum of the cubes of n
+e:
+2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
+
+p
+sum variable
+for loop that compares i with n
+statement that cubes
+return total
+
+What I googled:
+-how to cube a number
+questions:
+will input always be a positive integer?
+*/
