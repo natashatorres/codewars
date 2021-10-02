89fc0b269b2970531f23c3ec0ebc2d8c34b875fc
Natasha Torres <natashaxtorres@gmail.com>
2021-10-02 14:47:44 -0400

    updated after praciting

--- week4/helloNameWorld.js
+++ week4/helloNameWorld.js
@@ -1,7 +1,6 @@
 // //https://www.codewars.com/kata/57e3f79c9cb119374600046b
 
 function hello(name) {
-    const index = 0
     if(name){
         return 'Hello, ' + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + "!"
     }else{
