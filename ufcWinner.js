37623c230c34ed2f8f312bb392252528896e536c
Natasha Torres <natashaxtorres@gmail.com>
2021-10-03 19:55:21 -0400

    week 4 codewars

---
+++ ufcWinner.js
@@ -0,0 +1,11 @@
+
+//https://www.codewars.com/kata/582dafb611d576b745000b74
+
+
+var quote = function(fighter) {
+    if(fighter.toLowerCase() === "george saint pierre"){
+      return "I am not impressed by your performance."
+    }else{
+      return "I\'d like to take this chance to apologize.. To absolutely NOBODY!"
+    }
+  };
