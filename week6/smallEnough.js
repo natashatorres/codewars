c0644c1644fff4975f8c2c98523ec1d22eecd62e
Natasha Torres <natashaxtorres@gmail.com>
2021-10-12 20:32:24 -0700

    week 6 codewars

---
+++ week6/smallEnough.js
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/57cc981a58da9e302a000214
+
+function smallEnough(a, limit) {
+  for(let i = 0; i <= a.length; i++){
+    if(a[i] === limit && a[i] < limit){
+      return true
+    }else{
+      return false
+    }
+  }
+}
