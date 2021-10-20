5e37492f36a106976531368c7c62f3ed753e0f15
Natasha Torres <natashaxtorres@gmail.com>
2021-10-19 23:54:03 -0400

    week 7 codewars

--- week7/sumOfPos.js
+++ week7/sumOfPos.js
@@ -4,7 +4,6 @@ You get an array of numbers, return the sum of all of the positives ones.
 Note: if there is nothing to sum, the sum is default to 0.
 
 https://www.codewars.com/kata/5715eaedb436cf5606000381
-
 */
 
 function positiveSum(arr) {
