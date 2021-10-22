5a9d7a921130f8d5e08699cb9b257b8ce907ab01
Natasha Torres <natashaxtorres@gmail.com>
2021-10-21 20:52:55 -0400

    updated codewar

--- week1/youOnlyNeedOne.js
+++ week1/youOnlyNeedOne.js
@@ -1,10 +1,6 @@
 https://www.codewars.com/kata/57cc975ed542d3148f00015b
 
 function check(a, x) {
-    if(a.includes(x)){
-      return true;
-    }else{
-      return false;
-    }
+  return a.includes(x)
   }
 
