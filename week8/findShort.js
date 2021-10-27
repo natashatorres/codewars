9c0786c03d58aadec2cb93ef58eb951e6d2d2487
Natasha Torres <natashaxtorres@gmail.com>
2021-10-26 20:34:43 -0400

    week 8 codewars

---
+++ week8/findShort.js
@@ -0,0 +1,35 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+*/
+
+function findShort(s){
+}
+
+
+/*
+
+parameter: a string of words
+
+return:  return length of the shortest word(s)
+
+example: 
+"hello tash" ---> 4
+"goodbye see you soon" ---> 3
+
+
+
+questions:
+will the string ever be empty?
+will the string ever contain numbers?
+
+
+pseudocode
+create a new variable 'newArr' that combines the multidimensional array into one
+use concat and apply method
+concat adds the array's together (takes the multiple arguments, and merges all the arrays (and non-array arguments) into a single arra)
+use sort method for numeric values to order the array
+*/
+
