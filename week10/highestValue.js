5be2d2500447882f42c5774893a6c9d39f66ba3b
Natasha Torres <natashaxtorres@gmail.com>
2021-11-10 10:49:22 -0500

    week 10 codewars

--- week10/highestValue.js
+++ week10/highestValue.js
@@ -1,12 +1,13 @@
 function twoHighest(arr) {
-    let twoHigh = []
     let sorted = arr.sort(function (a, b) { return b - a })
-    if (sorted[0] === sorted[1]) {
-        sorted.shift(0)
-        return sorted.slice(0, 2)
-    } else {
-        return sorted.slice(0, 2)
-    }
+    let uniqueItems = [...new Set(sorted)]
+    return uniqueItems.slice(0, 2)
+    // if (sorted[0] === sorted[1]) {
+    //     sorted.shift(0)
+    //     return sorted.slice(0, 2)
+    // } else {
+    //     return sorted.slice(0, 2)
+    // }
 }
 
 //let sorted = arr.sort(function(a, b){return a - b});
