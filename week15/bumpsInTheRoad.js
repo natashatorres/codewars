01dfb494547795d9b73fa21f2ebc8df71d73f277
Natasha Torres <natashaxtorres@gmail.com>
2021-12-13 13:30:34 -0500

    week 15 codewars

---
+++ week15/bumpsInTheRoad.js
@@ -0,0 +1,26 @@
+/*
+
+Your car is old, it breaks easily. 
+The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+Unfortunately for you, your drive is very bumpy! 
+Given a string showing either flat road ("_") or bumps ("n"), work out if you make it home safely. 
+15 bumps or under, return "Woohoo!", over 15 bumps return "Car Dead".
+
+*/
+
+
+function bump(x){
+    let nCount = 0
+    //conditional statemennt 
+    for(let char of x){
+        if(char === "n"){
+            nCount += 1
+        }
+    }
+    return nCount <= 15 ? "Woohoo!" : "Car Dead"
+}
+
+
+console.log(bump("n"), "Woohoo!");
+console.log(bump("_nnnnnnn_n__n______nn__nn_nnn"), "Car Dead");
+console.log(bump("______n___n_"), "Woohoo!");
