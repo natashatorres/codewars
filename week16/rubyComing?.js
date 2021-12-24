62b84f23517d04f2cac84e88188153e39ed58f4a
Natasha Torres <natashaxtorres@gmail.com>
2021-12-23 21:21:45 -0500

    week 16 codewars

---
+++ week16/rubyComing?.js
@@ -0,0 +1,28 @@
+/*
+
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if at least one Ruby developer has signed up; or
+false if there will be no Ruby developers.
+For example, given the following input array:
+
+
+*/
+
+
+function isRubyComing(list) {
+    return list.some(e => e.language === 'Ruby')
+}
+  console.log(isRubyComing( [
+    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+    { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+  ]), true);
+  
+  console.log(isRubyComing([
+    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' }]
+    )
+    , false);
