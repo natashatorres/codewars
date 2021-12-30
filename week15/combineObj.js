f7fbc5b7a6b8f441524f4d8e0f094ae908e482b8
Natasha Torres <natashaxtorres@gmail.com>
2021-12-29 23:25:46 -0500

    updates

---
+++ week15/combineObj.js
@@ -0,0 +1,17 @@
+/*
+Your task is to write a function that takes two or more objects and returns a new object which combines all the input objects.
+All input object properties will have only numeric values. 
+Objects are combined together so that the values of matching keys are added together.
+*/
+
+
+function combine(...obj) {
+    const objA = { a: 10, b: 20, c: 30 }
+    const objB = { a: 3, c: 6, d: 3 }
+    const objC = { a: 5, d: 11, e: 8 }
+    const objD = { c: 3 }
+
+}
+
+
+console.log()
