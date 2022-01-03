6e611ad670dacc646b21f161fe21679b6a3f93cb
Natasha Torres <natashaxtorres@gmail.com>
2022-01-02 23:50:56 -0500

    week 18 codewars

---
+++ week18/continentsBeRep.js
@@ -0,0 +1,13 @@
+function allContinents(list) {
+    let continents = ['Africa', 'Americas', 'Asia', 'Europe',]
+}
+
+
+console.log(allContinents(
+    [
+    { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' },
+    { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
+    { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+    { firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
+    { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' },
+  ]), true)
