db29b18f21c641cd09a9c45786457139e3be9566
Natasha Torres <natashaxtorres@gmail.com>
2022-01-03 23:24:33 -0500

    week 18 codewars

---
+++ week18/createUserNames.js
@@ -0,0 +1,21 @@
+function addUsername(list) {
+//grab the first name turn it to lowercase
+//grab the the lastname first index and lowercase it
+//concat firstname, lastname first index and year
+// var dateOfBirth = new Date(new Date().getTime() - age); 
+
+list.forEach(e => e.username = `${e.firstName.toLowerCase()}${e.lastName[0].toLowerCase()}`)
+return list
+
+}
+
+console.log(addUsername([
+    { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby' },
+    { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure' }
+  ]), [
+    { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby', 
+      username: 'emilyn1990' },
+    { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure', 
+      username: 'nore2000' }
+  ])
+  
