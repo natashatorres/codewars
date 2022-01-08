c54d267257b6cdff0f183bfe6778b5caf73e124e
Natasha Torres <natashaxtorres@gmail.com>
2022-01-07 22:11:17 -0500

    updates

--- week12/spinWords.js
+++ week12/spinWords.js
@@ -7,26 +7,28 @@ Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" sp
 */
 
 
-function spinWords(str){
+
+function spinWords(str) {
     //variable turn the string that's passed through into a array
     let strArray = str.split(' ')
     //create an variable that holds an empty string
     let newStr = ""
+    let reversedWord = ""
     //loop that allows to check if a string contains words that more 5 characters
-    for(i = 0; i <= strArray.length; i++){
-      if(strArray[i].length > 5){
-        let reversedWord = strArray[i].split('').reverse().join('')
-        console.log(reversedWord)
-        newStr + reversedWord
-      }else{
-        newStr + strArray[i]
-      }
-      return newStr
+    for (let i = 0; i < strArray.length; i++) {
+        if (strArray[i].length >= 5) {
+            reversedWord = strArray[i].split('').reverse().join('')
+            newStr = newStr + " " + reversedWord
+        } else {
+            newStr = newStr + " " + strArray[i]
+        }
     }
+    return newStr.trim()
     //a conditional checks if word 5 characters
     //split and reversed and join 
     //else push string to empty variable that holds an empty string
-    }
-    
-    console.log(spinWords("Hey Catarina how are you"), "Hey aniratac how are you")
-    console.log(spinWords("This is another test"), "This is rehtona test")
+}
+
+console.log(spinWords("Hey Catarina how are you"), "Hey aniratac how are you")
+console.log(spinWords("This is another test"), "This is rehtona test")
+console.log(spinWords("Welcome"), "ecmocleW")
