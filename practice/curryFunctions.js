ab7b24edf3e6ba687d67965ce3fcb61d7bd33340
Natasha Torres <natashaxtorres@gmail.com>
2022-01-29 11:13:47 -0500

    codewars

---
+++ practice/curryFunctions.js
@@ -0,0 +1,18 @@
+/*
+
+To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers 
+as an argument. This function must return another function, which takes a single integer as an argument and 
+returns a new array. The returned array should consist of each of the elements from the first array multiplied 
+by the integer.
+
+*/
+
+function multiplyAll(arr){
+    arr.map(e => e * 2 )
+}
+
+
+/*
+
+
+*/
