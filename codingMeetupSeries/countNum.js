91c5740849dc9dadb0c7345828a5ac6702bc87e2
Natasha Torres <natashaxtorres@gmail.com>
2022-01-30 18:51:22 -0500

    codewars

---
+++ codingMeetupSeries/countNum.js
@@ -0,0 +1,46 @@
+/*
+
+
+You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+Your task is to return the number of JavaScript developers coming from Europe.
+
+For example, given the following list:
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+
+*/
+
+// function countDevelopers(list) {
+//     let count = 0
+
+//     list.find(element => {
+//         if (element.continent === 'Europe' && element.language === 'JavaScript') {
+//             count += 1
+//         } else {
+//             count = 0
+//         }
+//     })
+//     return count
+// }
+//or
+
+function countDevelopers(list) {
+   return list.filter(element => element.continent === 'Europe' && element.language === 'JavaScript').length
+}
+
+console.log(countDevelopers([
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+]), 1)
+
+console.log(countDevelopers([
+    { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 19, language: 'HTML' },
+    { firstName: 'Lukas', lastName: 'R.', country: 'Austria', continent: 'Europe', age: 89, language: 'HTML' }
+]), 0)
